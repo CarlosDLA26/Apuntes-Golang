5c87c793a73a1041d5c11a7879ac2d60788fd895
Carlos Loaiza <carlosdamian05329@gmail.com>
2023-12-08 21:34:19 -0500

    feat: agrega operaciones aritméticas

--- README.md
+++ README.md
@@ -4,7 +4,12 @@ Es un lenguaje de programación basado de código abierto creado por Google. Sus
 * Simplicidad: Go se inspira en lenguajes como C para el rendimiento, pero dejando de lado una sintaxis compleja.
 * Eficiencia: Su compilador ofrece código nativo lo que permite un rendimiento excelente. go posee un recolector de basura, por lo que los programadores no tienen que preocuparse por la administración de memoria.
 * Concurrencia y paralelismmo: Go proporciona goroutines que son unidades de ejecución livianas y canales para facilitar la comunicación entre ellas, lo cual es útil al momento de crear servicios web o servicios distribuidos.
+## Goroutines
+Las goroutines son lo que los lenguajes de programación denominan hilos. En el ecosistema de Go existe al menos una principal, llamada goruutina principal y puede controlar las demás goroutines. También existen los canales, que son formas de comunicarnos entre goroutines.
 ## Programación
 ### Crear build de Go
 Como Go es un lenguaje compilado debe crearse el ejecutable para poder usar el programa. Para ello usamos el comando `go build main.go`, esto creará un archivo `main.exe` podemos ejecutarlo dando doble click o desde la consola con el comando `./main.exe`.
-Otra forma de ejecutarlo es usando el comando `go run main.go`, esto hará que se compile el archivo go en una carpeta temporal, se ejecute y después se borre, lo caul ayuda a temas de debug y pruebas. Este método es menos eficiente.
+Otra forma de ejecutarlo es usando el comando `go run main.go`, esto hará que se compile el archivo go en una carpeta temporal, se ejecute y después se borre, lo cual ayuda a temas de debug y pruebas. Este método es menos eficiente.
+### Práctica
+Ver la sección BÁSICO y OPERACIONES ARITMÉTICAS del archivo `main.go`. Para la sección de operaciones aritméticas es posible usar este complemento [Cómo hacer cálculos matemáticos en Go con operadores](https://www.digitalocean.com/community/tutorials/how-to-do-math-in-go-with-operators-es).
+
--- curso_basico/main.go
+++ curso_basico/main.go
@@ -7,6 +7,87 @@ package main
 import "fmt"
 
 func main() {
+
+	/* ---------------------------BÁSICO--------------------------- */
+	// Declaración de constantes
+	const pi, pi2 = 3.1416, 3.15
+	const pi3 float64 = 3.1416
+
+	// Imprimir en consola
 	fmt.Println("Hola mundo")
+	fmt.Println("pi", pi)
+	fmt.Printf("pi2 %f", pi2)
+
+	// Al agregar := se dice que la variable no fue declarada
+	// antes, es decir, se declara por primera vez, ya después
+	// de que se declaró ya se puede usar solo el =
+	base := 20
+	fmt.Println(base)
+	base = 30
+	fmt.Println(base)
+
+	// Declaración de variables (me gusta más con el var)
+	base_1 := 20
+	var altura int = 34
+	var area int
+	var area_2 = 70
+	fmt.Println(base_1, altura, area, area_2)
+
+	// Zero values, Go asigna variables por defecto a tipos de variables
+	// específicas cuando no se inciializan con un valor preciso
+	var a int  // por defecto a es 0
+	var b float64  // por defecto es 0
+	var c string  // por defecto ""
+	var d bool  // por defecto false
+	fmt.Println(a, b, c, d)
+
+	// Ejemplo
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area del cuadrado", areaCuadrado)
+
+	/*---------------------------OPERACIONES ARITMÉTICAS---------------------------*/
+	var result = 0
+	// Suma
+	result = 20 + 10
+	fmt.Println("Suma:", result)
+	// Resta
+	result = 20 + 10
+	fmt.Println("Resta:", result)
+	// Multiplicación
+	result = 20 * 10
+	fmt.Println("Multiplicación:", result)
+	// Division
+	var result_1 float64 = 20 / 10.2
+	fmt.Println("División:", result_1)
+	// Módulo
+	var result_2 int = 21 % 2
+	fmt.Println("Módulo:", result_2)
+	// Incremental
+	var x = 0
+	x++
+	x += 2
+	fmt.Println("x:", x)
+	// Decremental
+	x--
+	x -= 3
+	fmt.Println("x:", x)
+	// Reto de areas
+	// Area rectangulo
+	h_rec := 20
+	b_rec := 10
+	fmt.Println("Area Rectángulo", h_rec * b_rec)
+	// Area de círculo
+	var r_cir float64 = 10
+	fmt.Println("Area círuclo", 3.1416 * r_cir * r_cir)  // Se debe decir que r_cir es float64
+	// Area Trapecio
+	var B_trap float64 = 20
+	var b_trap float64 = 10
+	var h_trap float64 = 2
+	fmt.Println("Area trapecio", ((b_trap * B_trap) / 2) * h_trap)
+
+	var xxx int = 10
+	yyy := 2.5 + 10
+	fmt.Println(xxx, yyy)
 
 }
